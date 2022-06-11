8600ffdb5807c8003af14f64670873296833e7df
Mark Gatere <gateremark5@gmail.com>
2022-06-12 01:50:53 +0300

    Added code to the file

---
+++ begin2.py
@@ -0,0 +1,17 @@
+#Attributes and Methods
+
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print("run")
+        return 0
+
+player1 = PlayerCharacter("Mark", 20)
+
+"""
+
+
+"""
